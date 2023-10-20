77e51bce42551854ec19703ff8cca7d66ab091bc
Sandeep Chatterjee <sndpchatterjee07@gmail.com>
2023-10-20 23:23:26 +0530

    Initial Git COmmit

---
+++ src/test/java/in/sandeep/SpringInterviewPro/SpringInterviewProApplicationTests.java
@@ -0,0 +1,13 @@
+package in.sandeep.SpringInterviewPro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInterviewProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
